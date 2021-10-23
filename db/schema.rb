a84fc24b862b162df9b742bda3095065ae6926cd
gohangasusumukun <isecha.ocha.cha.sket@gmail.com>
2021-10-23 07:12:00 +0000

    [add] topページ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_094209) do
+ActiveRecord::Schema.define(version: 2021_10_22_114507) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "category"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
